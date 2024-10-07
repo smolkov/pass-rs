41b758b7dcc1447fd9f4b506c192c41cac6893d1
smo <aleksandr.smolkov@sprecher-automation.com>
2024-10-07 15:46:51 +0200

    docs: add more todos.

--- src/secret.rs
+++ src/secret.rs
@@ -6,3 +6,13 @@ struct Secret {
 	user: String,
 	password: String
 }
+
+
+impl Secret {
+	pub fn new(user:&str, password: &str)  -> Secret {
+		Secret{
+			user: user.to_owned(),
+			password: password.to_owned(),
+		}
+	}
+}
---
+++ todo.md
@@ -0,0 +1,5 @@
+# Todo
+
+* [ ] generate autocompletion (see `broot` project in `.example` directory).
+* [ ] create gitlab action
+* [ ] create github action
